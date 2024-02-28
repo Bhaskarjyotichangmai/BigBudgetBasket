1e29c1426dc9a224057003051d5720fc169b31c5
Jahnab Borah <jahnab2342@gmail.com>
2024-02-28 21:01:48 +0530

    dash

--- src/app/home/Components/Hero.tsx
+++ src/app/home/Components/Hero.tsx
@@ -1,29 +1,74 @@
 import React from 'react'
-import Image from 'next/image';
+import Image, { StaticImageData } from 'next/image';
 import image from '../../Assets/image.png'
 import imagee from '../../Assets/imagee.png'
+import item1 from '../../Assets/image1.png'
+import item2 from '../../Assets/image5.png'
+import item3 from '../../Assets/image7.png'
+import item4 from '../../Assets/image11.png'
+import { FormControl,InputLabel,Select,MenuItem} from '@mui/material';
 
 interface Item {
     id: number;
     name: string;
-    description: string;
-    price: number;
+    image:StaticImageData;
+    imagee:StaticImageData;
 }
-function Hero({item}:{item:any}) {
+interface AdditionalItem {
+  id: number;
+  name: string;
+  quantity: number;
+  price: number;
+  image: string;
+}
+
+
+interface HeroProps {
+  item: Item;
+  additionalItems: AdditionalItem[];
+  handleQuantityChange: (itemId: number, event: React.ChangeEvent<{ value: unknown; }>) => void;
+}
+
+function Hero({item,additionalItems,handleQuantityChange}:HeroProps) {
   return (
-      <div className="item-card">
-        <div className='image-container'>
+      <div className="item-card p-4 flex justify-center">
+        
+            <div className='w-1/2 p-2'>
             <Image
               src={item.image}
               alt='image'
-              width={400}
-              height={200}
+              className='object-cover w-full h-full'
             />
-        </div>
-    <h2>{item.name}</h2>
-    <p>{item.description}</p>
-    <p>Price: {item.price}</p>
-</div>
+            </div>
+            <div className='w-1/2 p-2'>
+             <Image
+              src={item.imagee}
+              alt='imagee'
+              className='object-cover w-full h-full'
+             />
+             </div>
+             {additionalItems.map((additionalItem) => (
+                <div key={additionalItem.id} className='w-1/4 p-2'>
+                    <Image
+                        src={additionalItem.image}
+                        alt={additionalItem.name}
+                        className='object-cover w-full h-full'
+                    />
+                      <FormControl>
+                        <InputLabel id={`quantity-label-${additionalItem.id}`}>Qty</InputLabel>
+                        <Select
+                            labelId={`quantity-label-${additionalItem.id}`}
+                            value={additionalItem.quantity}
+                            onChange={(event: React.ChangeEvent<{ value: unknown }>) => handleQuantityChange(additionalItem.id, event)}
+                        >
+                            {[1, 2, 3, 4].map(quantity => (
+                                <MenuItem key={quantity} value={quantity}>{quantity}</MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+              </div>
+             ))}
+             </div>
   )
 }
 
